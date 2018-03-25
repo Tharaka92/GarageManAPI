feb59bae012ff10e990168a3638554b1d4deac86
Tharaka Herath <tharaka.career@gmail.com>
2018-03-25 22:56:55 +0530

    api response changes.

--- Breakdown/Breakdown.API/Controllers/v1/AccountController.cs
+++ Breakdown/Breakdown.API/Controllers/v1/AccountController.cs
@@ -117,7 +117,7 @@ namespace Breakdown.API.Controllers.v1
                 }
                 else
                 {
-                    return StatusCode(StatusCodes.Status200OK, new { IsSucceeded = result.Succeeded, Errors = result.Errors  });
+                    return StatusCode(StatusCodes.Status200OK, new { IsSucceeded = result.Succeeded, Error = result.Errors  });
                 }
             }
             catch (Exception ex)
