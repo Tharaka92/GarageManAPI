e6b7ace7fd73a0d11a6dc6372316c2285d4aea4a
Tharaka Herath <tashkraze@gmail.com>
2018-06-17 20:34:27 +0530

    PaymentController added.

---
+++ Breakdown/Breakdown.API/Controllers/v1/PaymentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Breakdown.Contracts.Braintree;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Breakdown.API.Controllers.v1
+{
+    [Produces("application/json")]
+    [Route("api/v1/[controller]/[action]")]
+    public class PaymentController : Controller
+    {
+        private readonly IBraintreeConfiguration _braintreeConfig;
+
+        public PaymentController(IBraintreeConfiguration braintreeConfig)
+        {
+            _braintreeConfig = braintreeConfig;
+        }
+
+        public async Task<IActionResult> GetToken()
+        {
+            try
+            {
+                return StatusCode(StatusCodes.Status200OK, new { Token = await _braintreeConfig.GenerateToken() });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+    }
+}
